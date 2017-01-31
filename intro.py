2569d46b6085c43556213de40674e07829429196
ChandanB <chandanbrown@yahoo.com>
2017-01-31 14:44:57 -0800

    Working Version Random Word Picker

--- intro.py
+++ intro.py
@@ -1,14 +1,14 @@
 import random
 import sys
 
+user_input = sys.argv[1:]
+words_file = open('/usr/share/dict/words')
 
-def random():
-    random_index = random.randint(1, len(sys.argv) - 1)
+def random_argument():
+    random_index = random.randint(1, words_file - 1)
     return sys.argv[random_index]
 
-
 if __name__ == '__main__':
-    for i in range(1, len(sys.argv)):
-        random_word = random()
+    for i in range(1, len(words_file):
+        random_word = random_argument()
         print(random_word + " ")
-
---
+++ randomWordDictionary.py
@@ -0,0 +1,4 @@
+import random
+
+user_input = sys.argv[1:]
+words_file = open('/usr/share/dict/words',r)
