f258ab315a1a3541c86b5ca359e7747c2868eb86
ChandanB <chandanbrown@yahoo.com>
2017-02-01 12:26:09 -0800

    This should update the git... I think lol

--- randomWordDictionary.py
+++ randomWordDictionary.py
@@ -1,4 +1,23 @@
 import random
+import sys
 
-user_input = sys.argv[1:]
-words_file = open('/usr/share/dict/words',r)
+words_file = open('/usr/share/dict/words')
+words_list = words_file.readlines()
+user_input = sys.argv[1]
+sentence   = []
+
+def get_word():
+        random_index = random.randint(1, len(words_list))
+        random_word = words_list[random_index]
+        return random_word
+
+if __name__ == '__main__':
+    count = int(user_input)
+    for i in range(1, count):
+        new_word = get_word()
+        word = new_word.strip()
+        sentence.append(word)
+    print (" ")
+    random_sentence =  " ".join(sentence)
+    print random_sentence + "."
+    print (" ")
