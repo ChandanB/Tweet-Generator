e714b24324168014475e7a55a876e67a0330cc82
ChandanB <chandanbrown@yahoo.com>
2017-02-14 12:32:27 -0800

    pushing for heroku dyno change

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def generate_sentence(num):
 if __name__ == '__main__':
     #uncomment to run on heroku
     # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port) 
     app.run(debug=True, port=33507)
 
     # uncomment to run locally
